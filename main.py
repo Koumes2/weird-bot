a88ab91e130764ec5dfeedce0df5297ecdf7310e
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-12-30 15:58:53 +0530

    chore: Introduce cog loading.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import disnake
 from disnake.ext import commands
 
+from dotenv import load_dotenv
+load_dotenv()
+
 import os, random, asyncio
 
 client = commands.Bot(
@@ -13,9 +16,52 @@ client = commands.Bot(
     help_command=None,
 )
 
+client.loadedcogs = []
+client.unloadedcogs = []
+
 @client.event
 async def on_ready():
-    print("The bot is now online. :weird:")
+
+    await client.change_presence(
+        status=disnake.Status.idle,
+        activity=disnake.Activity(
+            type=disnake.ActivityType.watching, name="the world burn."
+        ),
+    )
+
+    print("---------- COMMANDS ----------")
+    for filename in os.listdir("./commands"):
+        try:
+            if filename.endswith(".py"):
+                client.load_extension(f"commands.{filename[:-3]}")
+                client.loadedcogs.append(filename[:-3])
+                print(f"|  Loaded {filename}.")
+            else:
+                pass
+        except Exception as e:
+            print(f"|  Could not load {filename} due to exception: \n{e}")
+
+    print("----------- EVENTS -----------")
+    for filename in os.listdir("./events"):
+        try:
+            if filename.endswith(".py"):
+                client.load_extension(f"events.{filename[:-3]}")
+                client.loadedcogs.append(filename[:-3])
+                print(f"|  Loaded {filename}.")
+            else:
+                pass
+        except Exception as e:
+            print(f"|  Could not load {filename} due to exception: \n{e}")
+    
+    print("------------------------------")
+    print(f"|  Logged in as {client.user}.")
+
+@client.slash_command(
+    name="weird",
+    description="A random weird thing.",
+)
+async def weird(ctx):
+    await ctx.response.send_message("<:weird:925717694723002388>")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-bot.run(BOT_TOKEN)
+client.run(BOT_TOKEN)
